9f1b938f9449ba32bd2c99fbc223e3f23b22e80b
Maksim Shilov <storm25132513@gmail.com>
2018-05-26 08:52:43 +0300

    added setup.py

--- task-manager/cli/main.py
+++ task-manager/cli/main.py
@@ -1,12 +1,12 @@
-from arg_parser import *
+from cli.arg_parser import *
 from config.commands import *
 from config.session import *
 
 
-def main():
+def run():
     start_session()
     process_args()
 
 
 if(__name__ == "__main__"):
-    main()
+    run()
