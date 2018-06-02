96965bfa17e260041054f6172169aaa2d1f8eb93
Maksim Shilov <storm25132513@gmail.com>
2018-06-02 16:24:46 +0300

    added dependencies to setup.py

--- task-manager/console/setup.py
+++ task-manager/console/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='task-manager',
     version='0.1',
+    install_requires=['dateparser', 'humanize'],
     packages=find_packages(),
     entry_points='''
     [console_scripts]
--- task-manager/library/setup.py
+++ task-manager/library/setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='TaskManagerLibrary',
     version='0.1',
+    install_requires=['peewee'],
     packages=find_packages(),
     test_suite='tests.run')
