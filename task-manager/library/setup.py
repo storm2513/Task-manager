7e840c6475ba5948fd64118d78a5fc43d9f3b0fd
Maksim Shilov <storm25132513@gmail.com>
2018-06-08 20:46:31 +0300

    added dependency for factory boy

--- task-manager/library/setup.py
+++ task-manager/library/setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='TaskManagerLibrary',
     version='0.1',
-    install_requires=['peewee'],
+    install_requires=['peewee', 'factory_boy'],
     packages=find_packages(),
     test_suite='tests.run_tests')
